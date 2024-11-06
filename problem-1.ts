bd2b3fa9fb6751cbd511bc84eb709915e8833a8b
Dodul01 <mhdodul299@gmail.com>
2024-11-06 18:58:48 +0600

    Solution one added

---
+++ problem-1.ts
@@ -0,0 +1,9 @@
+const sumArray = (num: number[]): number => {
+    const sum = num.reduce((x, y) => x + y, 0);
+    return sum;
+}
+
+const array: number[] = [10, 50];
+
+sumArray(array);
+console.log(sumArray(array));
